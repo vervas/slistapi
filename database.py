52f60da67c19718f02b8ddd2b9ed24180e89d7e2
Dimitris Verraros <vervas.g@gmail.com>
2013-11-30 22:39:22 +0100

    Mongo

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 class MongoConnection():
-    def __init__(self, host='localhost', port=27017, db, collection):
+    def __init__(self, host='localhost', port=27017, db='', collection=''):
         client = MongoClient(host, port)
         self.db = client[db][collection]
--- model/list.py
+++ model/list.py
@@ -1,6 +1,7 @@
 from flask.ext.restful import reqparse, abort, Resource, fields, marshal_with
 from model.item import Item
 from database import MongoConnection
+from bson.objectid import ObjectId
 
 
 
@@ -15,39 +16,31 @@ parser.add_argument('name', type=str)
 
 resource_fields = {
     'name': fields.String,
-    #'items': fields.List(fields.Nested(Item))
+    'items': fields.List(fields.Nested(Item))
 }
-
 
 
 LISTS = MongoConnection(db='slistapi', collection='lists').db
 
 
-class ListDao(object):
-    def __init__(self, list_id, name):
-        self.list_id = list_id
-        self.name = name
-        self.its = {}
-
-
 class List(Resource):
     @marshal_with(resource_fields)
     def get(self, list_id):
-        abort_if_list_doesnt_exist(list_id)
-        return LISTS[list_id]
+        return LISTS.find_one({"_id": ObjectId(list_id)})
 
     def delete(self, list_id):
-        abort_if_list_doesnt_exist(list_id)
-        del LISTS[list_id]
+        LISTS.remove({"_id": ObjectId(list_id)})
         return '', 204
 
 
 class Lists(Resource):
     def get(self):
-        return LISTS
+        lists = {}
+        for list_ in LISTS.find():
+            lists[list_.pop("_id").__str__()] = list_
+        return lists
 
     def post(self):
         args = parser.parse_args()
-        list_id = 'list%d' % (len(LISTS) + 1)
-        LISTS[list_id] = {'name': args['name']}
-        return LISTS[list_id], 201
+        list_id = LISTS.insert({'name': args['name'], 'items': []})
+        return list_id.__str__(), 201
