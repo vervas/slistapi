2df4e31d5b9c994ed7c14259b5ce9c9356fd8d17
Dimitris Verraros <vervas.g@gmail.com>
2013-12-01 00:44:12 +0100

    Hacker

--- model/items.py
+++ model/items.py
@@ -1,5 +1,12 @@
-from flask.ext.restful import Resource, fields, marshal_with
+from flask.ext.restful import reqparse, abort, Resource, fields, marshal_with
+from model.lists import LISTS
+from bson.objectid import ObjectId
+from pymongo.errors import InvalidId
 
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str)
+parser.add_argument('priority', type=int)
 
 resource_fields = {
     'name': fields.String,
@@ -8,22 +15,33 @@ resource_fields = {
 
 
 class Item(Resource):
-    @marshal_with(resource_fields)
-    def get(self, **kwargs):
-        pass
+    def delete(self, list_id, name):
+        LISTS.update({'_id': ObjectId(list_id)},
+                {'$pull':
+                    {
+                        'items': {
+                            'name': name
+                        }
+                    }
+                },
+                upsert=False)
+        return '', 204
 
-    def delete(self, **kwargs):
-        pass
-
-    def post(self, **kwargs):
-        pass
-
-    def put(self, **kwargs):
-        pass
 
 class Items(Resource):
-    def get(self):
-        pass
+    def get(self, list_id):
+        return LISTS.find_one({"_id": ObjectId(list_id)})['items']
 
-    def post(self):
-        pass
+    def post(self, list_id):
+        args = parser.parse_args()
+        item = LISTS.update({'_id': ObjectId(list_id)},
+                {'$push':
+                    {
+                        'items': {
+                            'name': args['name'],
+                            'priority': args['priority']
+                        }
+                    }
+                },
+                upsert=False)
+        return item['updatedExisting'], 201
--- server.py
+++ server.py
@@ -9,7 +9,7 @@ api.add_resource(lists.Lists, '/lists')
 api.add_resource(lists.List, '/lists/<string:list_id>')
 
 api.add_resource(items.Items, '/lists/<string:list_id>/items')
-api.add_resource(items.Item, '/lists/<string:list_id>/items/<string:item_id>')
+api.add_resource(items.Item, '/lists/<string:list_id>/items/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
