36024e3cceafe58185ead1e5dc354b5aa27374e9
Dimitris Verraros <vervas.g@gmail.com>
2013-12-01 01:09:22 +0100

    Arsch

--- model/items.py
+++ model/items.py
@@ -1,4 +1,4 @@
-from flask.ext.restful import reqparse, abort, Resource, fields, marshal_with
+from flask.ext.restful import reqparse, abort, Resource
 from model.lists import LISTS
 from bson.objectid import ObjectId
 from pymongo.errors import InvalidId
@@ -8,40 +8,47 @@ parser = reqparse.RequestParser()
 parser.add_argument('name', type=str)
 parser.add_argument('priority', type=int)
 
-resource_fields = {
-    'name': fields.String,
-    'priority': fields.Integer
-}
-
 
 class Item(Resource):
     def delete(self, list_id, name):
-        LISTS.update({'_id': ObjectId(list_id)},
-                {'$pull':
-                    {
-                        'items': {
-                            'name': name
+        try:
+            LISTS.update({'_id': ObjectId(list_id)},
+                    {'$pull':
+                        {
+                            'items': {
+                                'name': name
+                            }
                         }
-                    }
-                },
-                upsert=False)
-        return '', 204
+                    },
+                    upsert=False)
+            return '', 204
+        except InvalidId:
+            abort(404, message="List {} doesn't exist".format(list_id))        
 
 
 class Items(Resource):
     def get(self, list_id):
-        return LISTS.find_one({"_id": ObjectId(list_id)})['items']
+        try:
+            return LISTS.find_one({"_id": ObjectId(list_id)})['items']
+        except InvalidId:
+            abort(404, message="List {} doesn't exist".format(list_id))        
 
     def post(self, list_id):
         args = parser.parse_args()
-        item = LISTS.update({'_id': ObjectId(list_id)},
-                {'$push':
-                    {
-                        'items': {
-                            'name': args['name'],
-                            'priority': args['priority']
+        try: 
+            item = LISTS.update({'_id': ObjectId(list_id)},
+                    {'$push':
+                        {
+                            'items': {
+                                '$each': [
+                                    {'name': args['name'], 'priority': args['priority']}
+                                ],
+                                '$sort': {'priority': 1},
+                                '$slice': -120
+                            }
                         }
-                    }
-                },
-                upsert=False)
-        return item['updatedExisting'], 201
+                    },
+                    upsert=False)
+            return item['updatedExisting'], 201
+        except InvalidId:
+            abort(404, message="List {} doesn't exist".format(list_id))        
